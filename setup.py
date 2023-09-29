362797d5e565f7c78a7fe86d0ee69a8c8ba5544b
orangetin <126978607+orangetin@users.noreply.github.com>
2023-09-29 13:35:07 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ ext_modules = []
 
 # Cache operations.
 cache_extension = CUDAExtension(
-    name="vllm.cache_ops",
+    name="vllm_cache_ops",
     sources=["csrc/cache.cpp", "csrc/cache_kernels.cu"],
     extra_compile_args={
         "cxx": CXX_FLAGS,
@@ -139,7 +139,7 @@ ext_modules.append(cache_extension)
 
 # Attention kernels.
 attention_extension = CUDAExtension(
-    name="vllm.attention_ops",
+    name="vllm_attention_ops",
     sources=["csrc/attention.cpp", "csrc/attention/attention_kernels.cu"],
     extra_compile_args={
         "cxx": CXX_FLAGS,
@@ -148,63 +148,63 @@ attention_extension = CUDAExtension(
 )
 ext_modules.append(attention_extension)
 
-# Positional encoding kernels.
-positional_encoding_extension = CUDAExtension(
-    name="vllm.pos_encoding_ops",
-    sources=["csrc/pos_encoding.cpp", "csrc/pos_encoding_kernels.cu"],
-    extra_compile_args={
-        "cxx": CXX_FLAGS,
-        "nvcc": NVCC_FLAGS,
-    },
-)
-ext_modules.append(positional_encoding_extension)
+# # Positional encoding kernels.
+# positional_encoding_extension = CUDAExtension(
+#     name="vllm.pos_encoding_ops",
+#     sources=["csrc/pos_encoding.cpp", "csrc/pos_encoding_kernels.cu"],
+#     extra_compile_args={
+#         "cxx": CXX_FLAGS,
+#         "nvcc": NVCC_FLAGS,
+#     },
+# )
+# ext_modules.append(positional_encoding_extension)
 
-# Layer normalization kernels.
-layernorm_extension = CUDAExtension(
-    name="vllm.layernorm_ops",
-    sources=["csrc/layernorm.cpp", "csrc/layernorm_kernels.cu"],
-    extra_compile_args={
-        "cxx": CXX_FLAGS,
-        "nvcc": NVCC_FLAGS,
-    },
-)
-ext_modules.append(layernorm_extension)
+# # Layer normalization kernels.
+# layernorm_extension = CUDAExtension(
+#     name="vllm.layernorm_ops",
+#     sources=["csrc/layernorm.cpp", "csrc/layernorm_kernels.cu"],
+#     extra_compile_args={
+#         "cxx": CXX_FLAGS,
+#         "nvcc": NVCC_FLAGS,
+#     },
+# )
+# ext_modules.append(layernorm_extension)
 
-# Activation kernels.
-activation_extension = CUDAExtension(
-    name="vllm.activation_ops",
-    sources=["csrc/activation.cpp", "csrc/activation_kernels.cu"],
-    extra_compile_args={
-        "cxx": CXX_FLAGS,
-        "nvcc": NVCC_FLAGS,
-    },
-)
-ext_modules.append(activation_extension)
+# # Activation kernels.
+# activation_extension = CUDAExtension(
+#     name="vllm.activation_ops",
+#     sources=["csrc/activation.cpp", "csrc/activation_kernels.cu"],
+#     extra_compile_args={
+#         "cxx": CXX_FLAGS,
+#         "nvcc": NVCC_FLAGS,
+#     },
+# )
+# ext_modules.append(activation_extension)
 
-# Quantization kernels.
-quantization_extension = CUDAExtension(
-    name="vllm.quantization_ops",
-    sources=[
-        "csrc/quantization.cpp",
-        "csrc/quantization/awq/gemm_kernels.cu",
-    ],
-    extra_compile_args={
-        "cxx": CXX_FLAGS,
-        "nvcc": NVCC_FLAGS,
-    },
-)
-ext_modules.append(quantization_extension)
+# # Quantization kernels.
+# quantization_extension = CUDAExtension(
+#     name="vllm.quantization_ops",
+#     sources=[
+#         "csrc/quantization.cpp",
+#         "csrc/quantization/awq/gemm_kernels.cu",
+#     ],
+#     extra_compile_args={
+#         "cxx": CXX_FLAGS,
+#         "nvcc": NVCC_FLAGS,
+#     },
+# )
+# ext_modules.append(quantization_extension)
 
-# Misc. CUDA utils.
-cuda_utils_extension = CUDAExtension(
-    name="vllm.cuda_utils",
-    sources=["csrc/cuda_utils.cpp", "csrc/cuda_utils_kernels.cu"],
-    extra_compile_args={
-        "cxx": CXX_FLAGS,
-        "nvcc": NVCC_FLAGS,
-    },
-)
-ext_modules.append(cuda_utils_extension)
+# # Misc. CUDA utils.
+# cuda_utils_extension = CUDAExtension(
+#     name="vllm.cuda_utils",
+#     sources=["csrc/cuda_utils.cpp", "csrc/cuda_utils_kernels.cu"],
+#     extra_compile_args={
+#         "cxx": CXX_FLAGS,
+#         "nvcc": NVCC_FLAGS,
+#     },
+# )
+# ext_modules.append(cuda_utils_extension)
 
 
 def get_path(*filepath) -> str:
